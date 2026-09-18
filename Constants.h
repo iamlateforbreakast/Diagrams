8e7df851b395f194e6eb468e0b062785d03b32de
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-09-18 17:44:19 +0100

    Create Constants.h

---
+++ Constants.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define FAILED (1)
+#define OK (0)
+
+#define ROOT_TYPE (1)
+#define BOX_TYPE (2)
+#define BOX_FILL_TYPE (3)
+#define PILL_TYPE (4)
+#define PILL_FILL_TYPE (5)
+#define LINE_TYPE (6)
+#define CIRCLE_TYPE (7)
+#define CIRCLE_FILL_TYPE (8)
+#define TEXTBOX_TYPE (9)
+
+#define IMG_WIDTH  (640)
+#define IMG_HEIGHT (480)
+#define CHANNELS   (3)
+
+#endif /* CONSTANTS_H */
